9a0408338144e5ccb1bd97d72c767e727615df1c
Oleg Zhuravlev <olezhuravlev@gmail.com>
2021-05-15 17:04:54 +0700

    ABS preheat temperature changed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1712,8 +1712,8 @@
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_TEMP_HOTEND 220
+#define PREHEAT_2_TEMP_BED    70
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
